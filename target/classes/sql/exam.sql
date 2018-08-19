87f9f8f0289f0c9573ed63331fe369947930733e
Ocean <Ocean@DESKTOP-TAFB420>
2018-08-19 10:08:51 +0900

    HIT상품 / NEW상품

--- target/classes/sql/exam.sql
+++ target/classes/sql/exam.sql
@@ -10,4 +10,5 @@ select p.*, sort_table.sumqt from TB_PRODUCT p, (
 select prd_no, sum(qty) sumqt from TB_ORDER_PRODUCT group by prd_no order by sumqt desc) sort_table
 where p.prd_no = sort_table.prd_no and rownum<=5
 	
-	
+select * from tb_product	
+select * from tb_order_product
